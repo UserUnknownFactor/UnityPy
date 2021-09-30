decb5f0dd779800214c6658808377c0fcf87aee8
UserUnknownFactor <63057995+UserUnknownFactor@users.noreply.github.com>
2021-09-30 12:39:12 +0300

    Fix enums with unknown IDs

--- UnityPy/enums/__init__.py
+++ UnityPy/enums/__init__.py
@@ -1,7 +1,6 @@
 from .Audio import AudioType, AudioCompressionFormat, AUDIO_TYPE_EXTEMSION
-from .BuildTarget import BuildTarget
-from .BundleFile import ArchiveFlags, ArchiveFlagsOld, CompressionFlags
-from .ClassIDType import ClassIDType
+from .BuildTarget import BuildTarget, makeBuildTarget
+from .ClassIDType import ClassIDType, makeClassID
 from .FileType import FileType
 from .TextureFormat import TextureFormat
 from .SpriteMeshType import SpriteMeshType
@@ -10,7 +9,5 @@ from .CommonString import CommonString
 from .ShaderCompilerPlatform import ShaderCompilerPlatform
 from .ShaderGpuProgramType import ShaderGpuProgramType
 from .SerializedPropertyType import SerializedPropertyType
-from .SpritePackingMode import SpritePackingMode
-from .SpritePackingRotation import SpritePackingRotation
 from .TextureDimension import TextureDimension
 from .PassType import PassType
